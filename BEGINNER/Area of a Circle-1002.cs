83c19790c3956f8a97e84ee0655dc905982b470d
khalledalkarmi <khalled_95@hotmail.com>
2020-07-15 18:15:42 +0300

    Create Area of a Circle-1002.cs

---
+++ BEGINNER/Area of a Circle-1002.cs
@@ -0,0 +1,14 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+           double r = double.Parse(Console.ReadLine());
+            double n = 3.14159;
+            double A = r * r * n;
+            Console.WriteLine("A="+A.ToString("0.0000"));
+
+    }
+
+}
